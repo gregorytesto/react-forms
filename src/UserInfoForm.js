ac17469787b201b619181d709a0ee4a5d90a29f3
Greg Testo <gregt@pursuit.org>
2021-11-09 14:26:54 -0500

    first commit

---
+++ src/UserInfoForm.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+class UserInfoForm extends Component {
+    render(){
+      return (
+        <div>User info form</div>
+      )
+    }
+}
+
+export default UserInfoForm;
