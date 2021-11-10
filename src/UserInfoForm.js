909bd05802c9646bb3b8174ec860f27cd076361c
Greg Testo <gregt@pursuit.org>
2021-11-10 12:09:32 -0500

    Added input field

--- src/UserInfoForm.js
+++ src/UserInfoForm.js
@@ -5,12 +5,23 @@ class UserInfoForm extends Component {
         super();
         this.state = {
             notARobot: false,
-            userTitle: ''
+            userTitle: '',
+            fullName: ''
         }
     }
 
     handleFormSubmit=(event)=>{
         event.preventDefault();
+        let { fullName, userTitle, notARobot } = this.state;
+
+        if (notARobot && userTitle && fullName) {
+          alert(
+            `Form submitted! \n ${notARobot} \n ${userTitle} \n ${fullName}`
+          );
+        } else {
+          alert("Please fill out the form completely");
+        }
+      
     }
 
     handleCheckboxChange=(event)=>{
@@ -25,31 +36,52 @@ class UserInfoForm extends Component {
         })
     }
 
+    handleFullNameChange=(event)=>{
+        // console.log(event.target.value)
+        this.setState({
+            fullName: event.target.value
+        })
+    }
+
     render(){
       
       return (
-        <form onSubmit={this.handleFormSubmit} className="form-container">
-            <h2>User Information</h2>
-            <label>
-                Are you human?
-                <input
-                    id="not-robot"
-                    type="checkbox"
-                    checked={this.state.notARobot}
-                    onChange={this.handleCheckboxChange}
-                />
-            </label>
-            <select value={this.state.userTitle} onChange={this.handleSelectChange}>
-                <option value=''>--Please select title--</option>
-                <option value='mr'>Mr.</option>
-                <option value="ms">Ms.</option>
-                <option value='mrs'>Mrs.</option>
-                <option value='mx'>Mx.</option>
-                <option value='dr'>Dr.</option>
-            </select>
+        <div>
+            <div>Current User</div>
+            <div>Title: {this.state.userTitle}</div>
+            <div>Full name: {this.state.fullName}</div>
+            <div>Are you a human?: {this.state.notARobot ? "True" : "False"}</div>
+            <form onSubmit={this.handleFormSubmit} className="form-container">
+                <h2>User Information</h2>
 
-            <button type="submit">Submit</button>
-        </form>
+                <label>
+                    Are you human?
+                    <input
+                        id="not-robot"
+                        type="checkbox"
+                        checked={this.state.notARobot}
+                        onChange={this.handleCheckboxChange}
+                    />
+                </label>
+                <select value={this.state.userTitle} onChange={this.handleSelectChange}>
+                    <option value=''>--Please select title--</option>
+                    <option value='mr'>Mr.</option>
+                    <option value="ms">Ms.</option>
+                    <option value='mrs'>Mrs.</option>
+                    <option value='mx'>Mx.</option>
+                    <option value='dr'>Dr.</option>
+                </select>
+
+                <input 
+                    type="text"
+                    placeholder="Please enter fullname"
+                    value={this.state.fullName}
+                    onChange={this.handleFullNameChange}
+                />
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
       )
     }
 }
